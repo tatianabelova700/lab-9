3cdab5ab158f0e7ef107a863e7f6e98b7458f495
Tatyana <Tatyana@DESKTOP-74DNP3C>
2021-12-19 16:28:18 +0300

    Добавьте файлы проекта.

---
+++ lab 9/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                long x;
+                float y;
+                char K;
+                double answer = 0;
+
+                Console.Write("x = ");
+                x = Convert.ToInt64(Console.ReadLine());
+                Console.Write("y = ");
+                y = Convert.ToSingle(Console.ReadLine());
+                Console.Write("operation (+, -, /, *, %) = ");
+                K = Convert.ToChar(Console.ReadLine());
+
+                switch (K)
+                {
+                    case '+':
+                        answer = x + y;
+                        break;
+                    case '-':
+                        answer = x - y;
+                        break;
+                    case '*':
+                        answer = x * y;
+                        break;
+                    case '/':
+                        answer = x / y;
+                        break;
+                    case '%':
+                        answer = x % y;
+                        break;
+                }
+
+                Console.WriteLine("Answer is {0}", answer.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
